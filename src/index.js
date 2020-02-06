d295337bd20296adc23639f3cd9177e2cd3eb64f
Thijs <35424969+hiphof@users.noreply.github.com>
2020-02-06 19:47:17 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import "./styles.css";
+
+var todolist = [
+  'program feature "make font bigger when opening in big screen"',
+  'program feature "add by ENTER"'
+];
+
+var todoitem = "make coffee";
+
+function addtodo() {
+  todolist.unshift(todoitem);
+  numberoftasks = todolist.length;
+  var inputvalue = (document.getElementById("input1").value = "");
+}
+
+function buttonfunction() {
+  var inputvalue = document.getElementById("input1").value;
+  todoitem = inputvalue;
+  addtodo();
+
+  filldiv();
+}
+
+function generate() {
+  var outputmessage = "";
+  for (var i = 0; i < numberoftasks; i++) {
+    outputmessage += "<li>" + todolist[i] + "</li>";
+  }
+  return outputmessage;
+}
+
+document.getElementById("button1").innerHTML = `add`;
+
+document.getElementById("button1").addEventListener("click", buttonfunction);
+
+var numberoftasks = todolist.length;
+
+function filldiv() {
+  document.getElementById("app").innerHTML =
+    `
+<div>
+  <ul>
+    ` +
+    generate() +
+    ` 
+  </ul>
+</div>
+`;
+}
+filldiv();
