39abb9e9c7b355d80ab6f53f04258ce39a309e51
Thijs <35424969+hiphof@users.noreply.github.com>
2020-02-11 15:34:26 +0100

    change position of Pro link

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import "./styles.css";
 
-var todolist = [];
+let todolist = [];
 
-var todoitem = [];
+let todoitem = [];
+
+function buttonfunction() {
+  let inputvalue = document.querySelector("#input1").value;
+  todoitem = inputvalue;
+
+  addtodo();
+
+  filldiv();
+}
 
 function addtodo() {
   todolist.unshift(todoitem);
   numberoftasks = todolist.length;
-  var inputvalue = (document.querySelector("#input1").value = "");
+  let inputvalue = (document.querySelector("#input1").value = "");
 }
 
 /*
-const node = document.getElementsByClassName(".inputclass")[0];
+const node = document.querySelector(".inputclass")[0];
 node.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
       console.log("hoi")  
@@ -20,27 +29,34 @@ node.addEventListener("keyup", function(event) {
 });
 */
 
-function completeTodo() {
-  console.log("clicked ");
-  //console.log(i);
-
-  document.querySelector("#app").innerHTML =
-    '<br>To mark a task complete, <strong><a href="https://bunq.me/open-request/t/472e6a4e-4c53-4522-aef2-ea38daa1ebaf">upgrade to the Pro version</a></strong>.';
+function updateEnd() {
+  const todos = document.querySelectorAll(".todo-item");
+  if (todos) {
+    console.log(todos);
+    for (let i = 0; i < todos.length; i++) {
+      todos[i].addEventListener("click", function() {
+        completeTodo(i);
+      });
+      console.log("i=" + i);
+    }
+  }
 }
 
-function buttonfunction() {
-  var inputvalue = document.querySelector("#input1").value;
-  todoitem = inputvalue;
-  addtodo();
-
-  filldiv();
+function completeTodo() {
+  let clickingID = "#nr" + arguments[0];
+  //console.log(clickingID);
+  document.querySelector(clickingID).innerHTML =
+    'To mark a task complete, <strong><a href="https://bunq.me/open-request/t/472e6a4e-4c53-4522-aef2-ea38daa1ebaf">upgrade to the Pro version</a></strong>.';
+  //"<li>hier staat nog iets" + todolist[i] + "</li>";
 }
 
 function generateList() {
-  var outputmessage = "";
-  for (var j = 0; j < numberoftasks; j++) {
+  let outputmessage = "";
+  for (let j = 0; j < numberoftasks; j++) {
     outputmessage +=
-      '<li><a title="j' +
+      '<li id="nr' +
+      j +
+      '"><a title="j' +
       j +
       '" class="todo-item" id=j' +
       j +
@@ -55,7 +71,7 @@ document.querySelector("#button1").innerHTML = `add task`;
 
 document.querySelector("#button1").addEventListener("click", buttonfunction);
 
-var numberoftasks = todolist.length;
+let numberoftasks = todolist.length;
 
 function filldiv() {
   document.querySelector("#app").innerHTML =
@@ -74,13 +90,5 @@ function filldiv() {
 }
 filldiv();
 
-function updateEnd() {
-  const todos = document.querySelectorAll(".todo-item");
-  if (todos) {
-    console.log(todos);
-    for (let i = 0; i < todos.length; i++) {
-      todos[i].addEventListener("click", completeTodo);
-      console.log("i=" + i);
-    }
-  }
-}
+//todo
+// * change some let into const
